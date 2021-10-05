70dad8d8948b433c0d9796ab5693646fb3e577fc
manhtdg <manh@gmail.com>
2021-10-05 15:20:42 +0700

    fix logistic

--- ml-basic/logistic/logistic_regression.py
+++ ml-basic/logistic/logistic_regression.py
@@ -20,16 +20,26 @@ def logistic_regression(X, y, epochs, learning_rate):
     while epoch < epochs:
         print("Epoch: ", epoch)
         epoch += 1
-        yhat = predict(Xbar, w)
-        w = w + (learning_rate * (yhat - y) * Xbar).T
-    
+        shuffle_data = np.random.permutation(len(X))
+        for id_data in shuffle_data:
+            yhat = predict(Xbar[id_data], w)
+            w = w + (learning_rate * (y[id_data] - yhat) *  Xbar[id_data]).T
+        
     return w
 
+def accuracy(X, y, w):
+    Xbar = np.concatenate((X, np.ones((len(X), 1))), axis = 1)
+    cnt = 0
+    for i in range(len(X)):
+        if (round(predict(Xbar[i], w)) == y[i]):
+            cnt += 1
+    return cnt / len(X)
 
 if __name__ == '__main__':
     file_name = 'data.csv'
     X, y = load_data(file_name)
-
-    w = logistic_regression(X, y, 1000, 0.01)
+    w = logistic_regression(X, y, 10000, 0.005)
     print(w)
+    print(accuracy(X, y, w))
+    print(len(X))
     
