f8108314ea7c50ea63aa732d5f52d2e54ba4ab90
Boo <CBG.hai.09@gmail.com>
2021-11-24 02:53:01 +0700

    fix some bug

--- ml-basic/classification-writting-number-using-torch/something-with-torch.py
+++ ml-basic/classification-writting-number-using-torch/something-with-torch.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import optim
 
 model = nn.Sequential(
     nn.Linear(28*28, 256),
@@ -26,4 +27,39 @@ class unknownModel(nn.Module):
         return y_hat
 
 model1 = unknownModel()
+loss  = nn.CrossEntropyLoss()
+
+params = model.parameters()
+optimizer = optim.SGD(params, lr = 1e-2)
+
+from torchvision import datasets, transforms
+from torch.utils.data import random_split, DataLoader
+
+train_data = datasets.MNIST('data', train = True, download = True, transform = transforms.ToTensor())
+
+train, val = random_split(train_data, [55000, 5000])
+
+train_loader = DataLoader(train, batch_size = 32)
+val_loader = DataLoader(val, batch_size = 32)
+
+number_epochs = 20
+
+for epoch in range(number_epochs):
+    losses = list()
+    for batch in train_loader:
+        x, y = batch
+        b = x.size(0)
+        x = x.view(b, -1)
+
+        y_hat = model(x)
+
+        J = loss(y_hat, y)
+
+        model.zero_grad()
+
+        J.backward()
+
+        optimizer.step()
+        losses.append(J.item)
+    print("Eps: ", epoch + 1, 'Train loss', torch.tensor(J).mean())
 print("compile successfully")
